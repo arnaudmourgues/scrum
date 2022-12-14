58dd8c68b08b9d1bebb97b89bd9ab1e08ef204fc
nathan-granes <nathan.granes@alumni.univ-avignon.fr>
2022-12-14 11:31:10 +0100

    Commit: implements creating folder output with every output files

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 using namespace std;
 
@@ -13,13 +17,12 @@ string replace_space_by_underscore(string str) {
     return str;
 }
 
-string pdf_to_variable(const string &pdf_file) {
-    string variable_name = replace_space_by_underscore(pdf_file);
-    string command = "pdftotext -colspacing 0.7 " + pdf_file + " " + variable_name + ".txt";
+string pdf_to_variable(const string &folder, const string &variable_name) {
+    string command = "pdftotext " + folder + variable_name + ".pdf " + folder + "outputs/" + variable_name + ".txt";
     system(command.c_str());
-    ifstream file(variable_name + ".txt");
+    ifstream file(folder + "outputs/" + variable_name + ".txt");
     string content((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
-    command = "rm " + variable_name + ".txt";
+    command = "rm " + folder + "outputs/" + variable_name + ".txt";
     system(command.c_str());
     return content;
 }
@@ -66,6 +69,18 @@ void read_line_by_line(const string &str, string &title, string &abstract) {
     }
 }
 
+int dirExists(const char *path) //RETURN 0 if dir doesn't exists, else return 1
+{
+    struct stat info;
+
+    if(stat( path, &info ) != 0)
+        return 0;
+    else if(info.st_mode & S_IFDIR)
+        return 1;
+    else
+        return 0;
+}
+
 void pdf_to_tab(const string &folder) {
     string command = "find " + folder + " -name '*.pdf' > pdf_files.txt";
     system(command.c_str());
@@ -75,23 +90,45 @@ void pdf_to_tab(const string &folder) {
     string line;
     string title;
     string abstract;
-    string output = "output" + folder + ".txt";
-    ofstream output_file(output);
+    int verif = dirExists(("./" + folder + "/outputs").c_str()); //check if output folder exists, if not create it, else, delete it then create it
+        if(verif == 0){
+        system(("mkdir ./" + folder + "/outputs").c_str());
+        }
+        else{
+            system(("rm -r ./" + folder + "/outputs").c_str());
+            system(("mkdir ./" + folder + "/outputs").c_str());
+        }
     while (getline(f, line)) {
-        string pdf_content = pdf_to_variable(line);
+        string name = line.erase(0, 13);
+        if (name.find(".pdf") != string::npos) {
+            name.erase(name.find(".pdf"), 4);
+        }
+        string output = "./" + folder + "/";
+        string pdf_content = pdf_to_variable(output, name);
         read_line_by_line(pdf_content, title, abstract);
+        output = output + "outputs/" + name + ".txt";
+        ofstream output_file(output);
         output_file << "FILE : " << replace_space_by_underscore(line) << "\n" << title << "\n" << abstract << "\n" << endl;
     }
     command = "rm pdf_files.txt";
     system(command.c_str());
-    cout << "FIN ! Le nom du fichier output est : " << output << endl;
+    cout << "FIN ! Le nom du fichier output est : "  << endl;
 }
 
 int main() {
     // ask for the folder name
     string folder;
-    cout << "Enter the folder name: ";
-    cin >> folder;
+    while(true){
+        cout << "Enter the folder name: ";
+        cin >> folder;
+        if(dirExists(folder.c_str()) == 1)
+            break;
+        else{
+            cout << "This folder doesn't exists, please enter a valid folder path\n";
+        }
+
+    }
+
     pdf_to_tab(folder);
     return 0;
 }
